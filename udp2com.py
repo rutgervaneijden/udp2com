7cc48c7cf9126383a54f10c15ca15ad498da7b1c
rutgervaneijden <63955794+rutgervaneijden@users.noreply.github.com>
2021-09-24 08:46:58 +0200

    Update udp2com.py
    filter functionality

--- udp2com.py
+++ udp2com.py
@@ -1,14 +1,24 @@
 import serial
 import socket
 
+UDP_host = '127.0.0.1'
 UDP_port = 55551
+
 COM_port = 'COM5'
 COM_baudrate= 9600
 
+sentences = ['RSA','THS','VDM','VTG']
+
+def filter(data):
+    if data[3:6].decode() in sentences:
+        COM.write(data)
+        print(data)
+
 UDP = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-UDP.bind((socket.gethostname(),UDP_port))
+UDP.bind((UDP_host,UDP_port))
+
 COM = serial.Serial(COM_port, COM_baudrate)
+
 while True:
     data,addr = UDP.recvfrom(1024)
-    COM.write(data)
-    print(data)
+    filter(data)
